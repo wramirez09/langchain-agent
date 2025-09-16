8e87d8e74ab291b74027264d1b62a4f82e493b19
Will Ramirez <wramirez1980@gmail.com>
2025-09-16 18:04:19 -0500

    chore: fix import

--- components/forgot-password-form.tsx
+++ components/forgot-password-form.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { cn } from '@/utils'
+import { cn } from '@/utils/cn'
 import { createClient } from '@/utils/client'
 import { Button } from '@/components/ui/button'
 import {
