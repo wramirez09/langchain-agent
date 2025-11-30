dbb69a00430752790b5d3df87ea6b7ef3453b497
Will Ramirez <wramirez1980@gmail.com>
2025-11-30 11:42:25 -0600

    revert

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,11 +1,4 @@
 import Stripe from "stripe";
 
-if (!process.env.STRIPE_SECRET_KEY) {
-    throw new Error('STRIPE_SECRET_KEY is not set in environment variables');
-}
-
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-    apiVersion: "2025-10-29.clover", // Use the latest API version
-});
-
+const stripe = process.env.STRIPE_SECRET_KEY ? new Stripe(process.env.STRIPE_SECRET_KEY) : null;
 export default stripe;
