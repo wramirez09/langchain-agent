e6c8f073305fdf790db49814bfc14e4e825b1048
Will Ramirez <wramirez1980@gmail.com>
2025-11-30 21:54:41 -0600

    chore: add debug route

---
+++ app/api/debug/route.ts
@@ -0,0 +1,9 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    return NextResponse.json({
+        stripeKeyDefined: !!process.env.STRIPE_SECRET_KEY,
+        vercelEnv: process.env.VERCEL_ENV,
+        stripeKeys: Object.keys(process.env).filter((k) => k.startsWith("STRIPE")),
+    });
+}
