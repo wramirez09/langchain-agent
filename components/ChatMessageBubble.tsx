8e554c377ef7f698ba621954dad82a867fe56511
Will Ramirez <wramirez1980@gmail.com>
2026-01-26 16:46:20 -0600

    chore: remove package not needed

--- components/ChatMessageBubble.tsx
+++ components/ChatMessageBubble.tsx
@@ -4,8 +4,7 @@ import React, { useEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
 import { Loader2 } from "lucide-react";
 import ReactMarkdown from "react-markdown";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { vscDarkPlus } from "react-syntax-highlighter/dist/cjs/styles/prism";
+
 interface MarkdownRendererProps {
   content: string;
 }
@@ -13,29 +12,10 @@ const MarkdownRenderer: React.FC<MarkdownRendererProps> = ({ content }) => {
   return (
     <div className="prose max-w-none">
       <ReactMarkdown
-        components={{
-          code({ node, className, children, ...props }) {
-            const match = /language-(\w+)/.exec(className || "");
-            return match ? (
-              <SyntaxHighlighter
-                style={vscDarkPlus}
-                language={match[1]}
-                PreTag="div"
-                {...props}
-              >
-                {String(children).replace(/\n$/, "")}
-              </SyntaxHighlighter>
-            ) : (
-              <code className={className} {...props}>
-                {children}
-              </code>
-            );
-          },
-        }}
       >
         {content}
       </ReactMarkdown>
-    </div>
+    </div >
   );
 };
 
