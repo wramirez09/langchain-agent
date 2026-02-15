676d10de164c0c28fa3f4362dcf0c3b54e0fd885
Will Ramirez <wramirez1980@gmail.com>
2026-02-15 17:42:05 -0600

    chore: update logo

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import logo from "@/public/images/logo-main.svg";
+import logo from "@/public/images/ndLogo.png";
 import * as React from 'react';
 import { LogoutButton } from "./logout-button";
 import ManageBillingButton from "./ui/ManageBillingButton";
@@ -10,7 +10,7 @@ const TopBar: React.FC = () => {
     return (
 
         <div className="grid grid-cols-[1fr,auto] gap-2 px-4 py-2 md:py-3 bg-gradient-light items-center z-50 border-b border-gray-200 shadow-md">
-            <div className="flex gap-4 flex-col md:flex-row md:items-center ml-2">
+            <div className="flex gap-6 flex-col md:flex-row md:items-center ml-2">
                 <a
                     href="/"
                     rel="noopener noreferrer"
@@ -22,7 +22,7 @@ const TopBar: React.FC = () => {
                         alt="NoteDoctor.Ai Logo"
                         className="h-5 md:h-8 w-auto"
                     />
-                    <span className="text-xs md:text-md text-dark font-bold">NoteDoctor.Ai</span>
+                    <span className="text-sm md:text-md text-dark font-bold">NoteDoctor.Ai</span>
                 </a>
             </div>
             <div className="flex-col items-center justify-end gap-2">
