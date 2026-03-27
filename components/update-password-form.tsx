b6c8c2e40d21163790a09b749bdb099c478b98be
Will Ramirez <wramirez1980@gmail.com>
2026-03-27 11:14:32 -0500

    chore: fix conflict

--- components/update-password-form.tsx
+++ components/update-password-form.tsx
@@ -71,16 +71,12 @@ function UpdatePasswordFormCore() {
 
             <div>
               <Label>Email</Label>
-<<<<<<< HEAD
               <Input
                 type="email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value.toLowerCase())}
                 placeholder="Enter your email"
               />
-=======
-              {email ?  <Input type="email" value={email || ""} disabled /> : <Input type="email" />}
->>>>>>> dev
             </div>
 
             <div>
